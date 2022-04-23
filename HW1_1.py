fc73055173a9afb1b4d97f22ccef58b50127dfd2
EternalX123 <94783050+EternalX123@users.noreply.github.com>
2022-04-23 11:29:07 +0800

    Update HW1_1.py
    add more functions

--- HW1_1.py
+++ HW1_1.py
@@ -53,8 +53,9 @@ for i in soup.find_all("div"):
 for k in range(len(name)):
     ResizeImage(k,500,filein = 'D:\\python_and_ML\\'+str(k)+'.png')
     ResizeImage(k,50,filein = 'D:\\python_and_ML\\'+'type'+str(k)+'.png')
+
 k=0
-def hit_me():
+def getmap():
     global k
     image_name = str(k)+'.png'
     image_name1 = 'type'+str(k)+'.png'
@@ -76,15 +77,39 @@ def hit_me():
     n.config(text=name1[k])
     k+=1
  
+def lastpage():
+    global k
+    k-=2
+    image_name = str(k)+'.png'
+    image_name1 = 'type'+str(k)+'.png'
+    imag = cv2.imread(image_name)
+    imag1 = cv2.imread(image_name1)
+    b,g,r = cv2.split(imag)
+    img = cv2.merge((r,g,b))
+    b,g,r = cv2.split(imag1)
+    img1 = cv2.merge((r,g,b))
+    im = Image.fromarray(img)
+    im1 = Image.fromarray(img1)
+    imgtk = ImageTk.PhotoImage(image=im) 
+    imgtk1 = ImageTk.PhotoImage(image=im1) 
+    l.config(image = imgtk)
+    l.image = imgtk
+    ptype.config(image = imgtk1)
+    ptype.image = imgtk1
+    m.config(text=name[k])
+    n.config(text=name1[k])
+    k+=1
 
 var = StringVar()    # 将label标签的内容设置为字符类型，用var来接收hit_me函数的传出内容用以显示在标签上
 l = Label(root, textvariable = var, font=('Arial', 12))
 ptype = Label(root)
 m = Label(root) 
 n=Label(root)
-b = Button(root, text='获取最新谱面信息', font=('Arial', 14), width=14, height=1, command=hit_me)
+b = Button(root, text='获取最新谱面信息', font=('Arial', 14), width=14, height=1, command=getmap)
+b1=Button(root,text='上一页',font=('Arial', 14), width=14, height=1, command=lastpage)
 
 b.grid(column=1, row=0)
+b1.grid(column=0,row=0)
 m.grid(column=1, row=1)
 n.grid(column=1, row=2)
 ptype.grid(column=1, row=3)
