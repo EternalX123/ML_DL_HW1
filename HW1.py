9051b6570666c922c3fd3f1dfe347fd648e3f05e
EternalX123 <94783050+EternalX123@users.noreply.github.com>
2022-04-23 10:42:20 +0800

    Update HW1.py
    add more information & changes the picture size

--- HW1.py
+++ HW1.py
@@ -5,6 +5,7 @@ from tkinter import *
 from PIL import Image, ImageTk
 import cv2
 import numpy as np
+from HW1_2 import ResizeImage
 
 
 def download_img(url_info):
@@ -35,37 +36,58 @@ r = requests.get(url, headers = headers)
 soup = BeautifulSoup(r.text)
 j=0
 imag=[]
+imag1=[]
 name=[]
+name1=[]
 for i in soup.find_all("div"):
     try:
         if i['class'][0]=='g_map':
             imag.append(download_img([i.find('a').find_all("div")[0].get('style')[22:-1],str(j)+".png"]))
+            imag1.append(download_img(['https://m.mugzone.net/'+i.find('img').get('src'),'type'+str(j)+".png"]))
             name.append(i.find_all('p')[0].string)
+            name1.append(i.find_all('p')[1].string)
             j+=1
     except:
         1
+
+for k in range(len(name)):
+    ResizeImage(k,500,filein = 'D:\\python_and_ML\\'+str(k)+'.png')
+    ResizeImage(k,50,filein = 'D:\\python_and_ML\\'+'type'+str(k)+'.png')
 k=0
 def hit_me():
     global k
     image_name = str(k)+'.png'
+    image_name1 = 'type'+str(k)+'.png'
     imag = cv2.imread(image_name)
+    imag1 = cv2.imread(image_name1)
     b,g,r = cv2.split(imag)
     img = cv2.merge((r,g,b))
+    b,g,r = cv2.split(imag1)
+    img1 = cv2.merge((r,g,b))
     im = Image.fromarray(img)
+    im1 = Image.fromarray(img1)
     imgtk = ImageTk.PhotoImage(image=im) 
+    imgtk1 = ImageTk.PhotoImage(image=im1) 
     l.config(image = imgtk)
     l.image = imgtk
+    ptype.config(image = imgtk1)
+    ptype.image = imgtk1
     m.config(text=name[k])
+    n.config(text=name1[k])
     k+=1
  
 
 var = StringVar()    # 将label标签的内容设置为字符类型，用var来接收hit_me函数的传出内容用以显示在标签上
 l = Label(root, textvariable = var, font=('Arial', 12))
-
-m = Label(root)
+ptype = Label(root)
+m = Label(root) 
+n=Label(root)
 b = Button(root, text='获取最新谱面信息', font=('Arial', 14), width=14, height=1, command=hit_me)
+
 b.grid(column=1, row=0)
 m.grid(column=1, row=1)
-l.grid(column=1, row=2)
+n.grid(column=1, row=2)
+ptype.grid(column=1, row=3)
+l.grid(column=1, row=4)
 
 root.mainloop()
